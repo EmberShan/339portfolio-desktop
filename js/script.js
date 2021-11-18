43efdf61312204995a3e0db06f5d89b610441568
ember2000 <zshan@email.wm.edu>
2021-11-18 14:33:26 -0500

    reduced motion addded

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ const Links = document.getElementsByClassName('navlinks')[0]
 toggleButton.addEventListener('click', () => {
     Links.classList.toggle('active')
 })
+
+// impressionism scroll animation
+const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        // if reduced motion then don't perform the scroll animation
+        if (!(!mediaQuery || mediaQuery.matches)){
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    // console.log(mediaQuery)
+    });
+  });
